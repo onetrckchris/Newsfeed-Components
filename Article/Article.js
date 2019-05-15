224f66b89dfd71eac2312a144f43fad26a53a309
Chris Pelayo <chris.e.pelayo@gmail.com>
2019-05-15 13:38:39 -0700

    added animations to articles. Now open with GSAP

--- Article/Article.js
+++ Article/Article.js
@@ -8,7 +8,19 @@ class Article {
     this.expandButton.textContent = '↓';
     // this.expandButton.addEventListener('click', expandArticle);
     this.expandButton.addEventListener('click', () => {
-      oldArticle.classList.toggle('article-open');
+      // oldArticle.classList.toggle('article-open');
+      
+      if(oldArticle.classList[1] != 'toggle-open') {
+        TweenMax.to(oldArticle, 0.5, {height: 400})
+        oldArticle.classList.add('toggle-open');
+        console.log(oldArticle)
+      } else {
+        TweenMax.to(oldArticle, 0.5, {height: 70})
+        oldArticle.classList.remove('toggle-open');
+        console.log(oldArticle.classList[1])
+      }
+      
+
       this.expandButton.classList.toggle('article-open--button')
 
       if(this.expandButton.textContent === '↓') {
@@ -29,6 +41,5 @@ class Article {
 
 let articles = document.querySelectorAll('.article');
 articles.forEach((oldArticle) => {
-  console.log(oldArticle);
   const article = new Article(oldArticle);
 })
