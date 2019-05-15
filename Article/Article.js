6f0d238d78b7c9048a13aab85a30b3235f26264a
Chris Pelayo <chris.e.pelayo@gmail.com>
2019-05-15 15:36:37 -0700

    added animation to menu and fixed height on articles

--- Article/Article.js
+++ Article/Article.js
@@ -8,21 +8,20 @@ class Article {
     this.expandButton.textContent = '↓';
     // this.expandButton.addEventListener('click', expandArticle);
     this.expandButton.addEventListener('click', () => {
-      // oldArticle.classList.toggle('article-open');
-      
+
       if(oldArticle.classList[1] != 'toggle-open') {
-        TweenMax.to(oldArticle, 0.5, {height: 400})
+        TweenMax.set(oldArticle, {height: 'auto'});
+        TweenMax.from(oldArticle, 0.5, {height: 0});
         oldArticle.classList.add('toggle-open');
-        console.log(oldArticle)
+        TweenMax.to(menu, 0.15, {width: 0});
+        TweenMax.to(menuList, 0.15, {opacity: 0});
+        menu.classList.remove('menu-open');
       } else {
         TweenMax.to(oldArticle, 0.5, {height: 70})
         oldArticle.classList.remove('toggle-open');
-        console.log(oldArticle.classList[1])
       }
-      
 
       this.expandButton.classList.toggle('article-open--button')
-
       if(this.expandButton.textContent === '↓') {
         this.expandButton.textContent = '↑';
       } else {
@@ -33,9 +32,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    // this.expandButton.addEventListener('click', () => {
-    //   oldArticle.classList.toggle('article-open');
-    // });
+    
   }
 }
 
